8358bdc0e1c27eda4358bd77a0e919a2dc46ff5e
Tarun15082003 <nagireddy_2101cs48@iitp.ac.in>
2025-06-30 14:36:58 +0530

    My first Commit

---
+++ js/Demo.js
@@ -0,0 +1,28 @@
+// This is a single-line comment.
+/*
+This is a multi-line comment.
+It can span multiple lines.
+*/
+
+// Declare a variable and assign a value
+let message = "Hello, JavaScript!";
+
+// Log the message to the console
+console.log(message);
+
+// Perform a simple arithmetic operation
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2;
+
+// Log the result of the operation
+console.log("The sum is: " + sum);
+
+// Define a function
+function greet(name) {
+  return "Greetings, " + name + "!";
+}
+
+// Call the function and log the returned value
+let greetingMessage = greet("User");
+console.log(greetingMessage);
